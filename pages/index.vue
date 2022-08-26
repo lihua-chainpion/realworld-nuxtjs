4b67f32d1424d7484205243a9f2f643c06fe5d62
lihuabing <2946632291@qq.com>
2022-08-26 23:59:57 +0800

    init project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-page">
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style lang="less" scoped>
+.home-page {
+  color: chocolate;
+}
+</style>
